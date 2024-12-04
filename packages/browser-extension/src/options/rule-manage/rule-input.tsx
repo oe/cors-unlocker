45890f706cb758ab70045bf1aeb2f783e95acb14
Saiya <okarl@outlook.com>
2024-12-04 23:27:50 +0800

    feat: complete the add form

--- packages/browser-extension/src/options/rule-manage/rule-input.tsx
+++ packages/browser-extension/src/options/rule-manage/rule-input.tsx
@@ -1,3 +1,5 @@
+import { IRuleItem } from '@/types';
+import { Link, MessageSquareMore } from 'lucide-react';
 import { useState, useEffect } from 'react';
 
 export interface IRuleInputProps {
@@ -16,6 +18,11 @@ export function RuleInput(props: IRuleInputProps) {
   const [value, setValue] = useState(props.value || '');
   const [status, setStatus] = useState<InputStatus>({status: 'idle', message: ''});
 
+  /**
+   * validate the url
+   * @param url url user input
+   * @returns origin if valid, or an empty string
+   */
   const validateRule = (url: string) => {
     let origin = '';
     try {
@@ -31,17 +38,26 @@ export function RuleInput(props: IRuleInputProps) {
     return origin
   }
 
+  /**
+   * make it a controlled input
+   */
   useEffect(() => {
     const v = props.value || '';
     setValue(v);
-    validateRule(v);
   }, [props.value]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
+  /**
+   * validate the rule when
+   * * value changes
+   * * validateRule changes(when the rule is updated / removed)
+   */
+  useEffect(() => {
     let origin = validateRule(value);
+    props.onChange(value, origin);
+  }, [value, props.validateRule]);
+  
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
-    props.onChange(e.target.value, origin);
   };
 
   const statusClass = status.status === 'valid'
@@ -49,13 +65,12 @@ export function RuleInput(props: IRuleInputProps) {
     : status.status === 'invalid' ? 'text-red-500' : '';
 
   return (
-    <div className='flex-grow mr-2'>
-      <input
-        type="text"
+    <div className='flex-grow'>
+      <InputWithAddOn
         value={value}
+        prepend={<Link className='w-4' />}
+        onChange={onChange}
         placeholder='Enter an origin or an url'
-        onChange={handleChange}
-        className="border p-2 rounded-sm w-full"
         onKeyUp={props.onKeyUp}
       />
       <div className={`${statusClass} text-sm h-6 pl-2`}>{status.message}</div>
@@ -64,30 +79,92 @@ export function RuleInput(props: IRuleInputProps) {
 }
 
 export interface IAddRuleProps {
+  rule?: IRuleItem;
   validateRule: (url: string) => string;
   addRule: (url: string) => void;
 }
 
 export function AddRuleForm(props: IAddRuleProps) {
-  const [value, setValue] = useState({value: '', origin: ''});
+  const [formData, setFormData] = useState(() => getDefaultFormData(props.rule));
 
-  const handleAdd = () => {
-    if (!value.origin) return;
-    props.addRule(value.origin);
-    setValue({value: '', origin: ''});
+  const handleSave = () => {
+    if (!formData.origin) return;
+    setFormData(() => getDefaultFormData());
+    setTimeout(() => props.addRule(formData.origin), 0);
   }
 
+  const setFormValue = (val: Record<string, string>) => {
+    setFormData((prev) => ({...prev, ...val}));
+  }
+
+  const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== 'Enter') return;
+    const current = e.currentTarget;
+    const form = current.form;
+    if (!form) return;
+    const index = Array.prototype.indexOf.call(form.elements, current);
+    const nextInput = form.elements[index + 1];
+    if (nextInput && nextInput.tagName === 'INPUT') {
+      (nextInput as HTMLInputElement).focus();
+    } else {
+      handleSave();
+    }
+  };
+
+
   return (
-    <div className="mt-4 flex">
+    <form className="mt-4 flex flex-col">
       <RuleInput
-        value={value.value}
+        value={formData.url}
         validateRule={props.validateRule}
-        onChange={(v, o) => setValue({value: v, origin: o})}
-        onKeyUp={(e) => { if (e.key === 'Enter') handleAdd(); }}
+        onChange={(v, o) => setFormValue({url: v, origin: o})}
+        onKeyUp={onKeyUp}
       />
-      <button onClick={handleAdd} disabled={!value.origin} className="bg-blue-500 disabled:bg-blue-300 text-white px-4 py-2 rounded-sm self-baseline">
-        Add
+
+      <InputWithAddOn
+        className='mb-4'
+        value={formData.comment}
+        prepend={<MessageSquareMore className='w-4'/>}
+        onKeyUp={onKeyUp}
+        onChange={(e) => setFormValue({ comment: e.target.value })}
+        placeholder='comment(optional)' />
+
+      <button type="button" onClick={handleSave} disabled={!formData.origin} className="bg-blue-500 disabled:bg-blue-300 text-white px-4 py-2 rounded-md self-baseline">
+        { props.rule ? 'Save' : 'Add' }
       </button>
-    </div>
+    </form>
   )
+}
+
+
+interface IInputWithAddOnProps {
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyUp?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  prepend?: React.ReactNode;
+  className?: string;
+}
+
+function InputWithAddOn(props: IInputWithAddOnProps) {
+  return (
+    <div className={"flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-blue-500 " + (props.className || '')}>
+      {props.prepend && <div className="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">{props.prepend}</div>}
+      <input type="text" value={props.value} onChange={props.onChange}
+      onKeyUp={props.onKeyUp}
+      className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder={props.placeholder} />
+    </div>
+
+  )
+}
+
+function getDefaultFormData(rule?: IRuleItem) {
+  if (rule) {
+    return {
+      url: rule.origin,
+      origin: rule.origin,
+      comment: rule.comment
+    }
+  }
+  return { url: '', origin: '', comment: ''};
 }
