85f6b6ff0339283cb271761a3538a53ad50bc150
oe <okarl@outlook.com>
2025-01-10 16:04:23 +0000

    oe published a site update

--- packages/browser-extension/src/options/settings/index.tsx
+++
@@ -1,8 +0,0 @@
-
-export function Settings() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Settings</h1>
-    </div>
-  )
-}
--- readme.md
+++
@@ -1,4 +0,0 @@
-# Browser App CORS Enabler
-
-This is a simple browser extension that can enable web app CORS for all requests.
-
