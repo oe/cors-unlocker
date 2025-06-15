cd7a0eeadc4ed1b52dc5c0a0c612ad30615ed2dd
Saiya <okarl@outlook.com>
2025-06-15 21:58:46 +0800

    feat: update playground page style

--- packages/website/src/components/playground/index.tsx
+++ packages/website/src/components/playground/index.tsx
@@ -33,7 +33,7 @@ export const PlaygroundPage: React.FC = () => {
       </div>
 
       {/* Main Content */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         {/* Extension Status Section */}
         <ExtensionStatusCard vm={vm} />
 
